caf0332cc7b11d08a0edf0c9e2d14778d943a1cd
Aiman Anizan <anizanaimann1@gmail.com>
2021-10-19 23:03:27 +0800

    Define router handler

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const server = http.createServer(app);
+const port = 3000;
+const server = http.createServer(app); // initialize app to supply to HTTP server
 const { Server } = require('socket.io');
 const io = new Server(server);
+
+// define router handler
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// make server to listen 3000 port
+server.listen(port, () => {
+    console.log(`listening on: http://localhost:${port}\n`);
+});
